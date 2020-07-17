97361092448913d1d70f7c374737cabec6c4704d
frankstrater <frankstrater@gmail.com>
2020-07-17 14:15:40 +0200

    Add browser recommendation notice

---
+++ assets/js/common.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+	const isChrome = () => navigator.userAgent.indexOf('Chrome') > -1;
+	const cookieExists = (cookieName) => !!document.cookie.split(';').filter((item) => item.trim().startsWith(cookieName + '=')).length;
+	const setCookies = (cookieName) => {
+		const expiryDate = new Date();
+		expiryDate.setMonth(expiryDate.getMonth() + 1);
+		document.cookie = cookieName + '=accepted; expires=' + expiryDate;
+	};
+
+	if(!isChrome() && !cookieExists('bg__browser-recommendation')) {
+		$("#browser-recommendation").toast('show');
+	};
+
+	$('#browser-recommendation').on('hidden.bs.toast', function () {
+		setCookies('bg__browser-recommendation');
+	  $("#browser-recommendation").toast('dispose');
+	});
+
+});
