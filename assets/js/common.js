790fd418f45e4ea83edc758b249b8b9a95d1ac64
frankstrater <frankstrater@gmail.com>
2020-07-21 12:22:45 +0200

    Add user-consent js event

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,22 +1,45 @@
 $(document).ready(function() {
 
+	function isCookieSet(cookieName) {
+		return document.cookie.split(';').filter(item => item.trim().startsWith(cookieName + '=')).length > 0;
+	}
+
+	function setCookie(cookieName, intMonth) {
+		var expiryDate = new Date();
+		expiryDate.setMonth(expiryDate.getMonth() + intMonth);
+		document.cookie = cookieName + '=accepted; expires=' + expiryDate + '; path=/';
+	}
+
 	// Browser recommendation
 
-	const isChrome = () => navigator.userAgent.indexOf('Chrome') > -1;
-	const cookieExists = (cookieName) => !!document.cookie.split(';').filter((item) => item.trim().startsWith(cookieName + '=')).length;
-	const setCookies = (cookieName) => {
-		const expiryDate = new Date();
-		expiryDate.setMonth(expiryDate.getMonth() + 1);
-		document.cookie = cookieName + '=accepted; expires=' + expiryDate;
-	};
-
-	if(!isChrome() && !cookieExists('bg__browser-recommendation')) {
+	if((navigator.userAgent.indexOf('Chrome') < 0) && !isCookieSet('bg__browser-recommendation')) {
 		$("#browser-recommendation").toast('show');
 	};
 
 	$('#browser-recommendation').on('hidden.bs.toast', function () {
-		setCookies('bg__browser-recommendation');
+		setCookie('bg__browser-recommendation', 1);
 	  $("#browser-recommendation").toast('dispose');
 	});
+
+	// User consent
+
+	$('#menu_login a.nav-link').click(function(e) {
+    e.preventDefault();
+
+    if (!isCookieSet('bg__user-consent')) {
+    	$('#user-consent').modal('show');
+    } else {
+    	window.location = $(this).attr('href');
+    };
+  });
+
+  $('#btn-accept-user-consent').click(function(e) {
+    e.preventDefault();
+
+    setCookie('bg__user-consent', 6);
+
+    var href = $('#menu_login a.nav-link').attr('href');
+    window.location = href;
+  });
 
 });
